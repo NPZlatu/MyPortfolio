2eb4b8ed7f2c1f5840c288bad1fa3ca955daa4d1
Niraj Paudel <itsmenirajpaudel@gmail.com>
2024-04-01 17:17:51 +0545

    Add changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-export default {
+const config = {
+  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-    }),
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "" : "/",
+      relative: false,
+    },
   },
 };
+
+export default config;

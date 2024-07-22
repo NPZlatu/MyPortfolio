7259a11e09efea1a2c81605968aa55b9a44b3c28
Niraj Paudel <itsmenirajpaudel@gmail.com>
2024-07-22 13:41:11 +0545

    Add two articles: Protobuf, gRPC and enabled Elon Musk article

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -6,6 +6,8 @@
   import proj5Image from "$lib/images/proj_5.webp";
   import proj6Image from "$lib/images/proj_6.webp";
   import proj7Image from "$lib/images/proj_7.webp";
+  import protoBufImage from "$lib/images/protobuf.webp";
+  import grpcImage from "$lib/images/grpc.webp";
 
   let articles = [
     {
@@ -17,20 +19,13 @@
     },
     {
       title:
-        "Functional Programming for Beginners: Understanding the Core Principles",
-      imageUrl: proj1Image,
-      description:
-        "This guide introduces beginners to the principles of Functional Programming (FP), emphasizing the utilization of pure functions, immutability, and function composition for writing cleaner and more maintainable code.",
-      url: "https://medium.com/@nirajpaudel/functional-programming-for-beginners-understanding-the-core-principles-fa8df00966f7",
-    },
-    {
-      title:
         "Understanding Rust's Memory Management: Ownership, Borrowing, and Lifetimes",
       imageUrl: proj2Image,
       description:
         "Rust's memory management, facilitated by its Borrow Checker, ensures both memory safety and performance by enforcing rules during compilation, eliminating runtime overhead associated with garbage collection or manual memory allocation/deallocation.",
       url: "https://www.linkedin.com/pulse/understanding-rusts-memory-management-ownership-borrowing-paudel-izwhc/?trackingId=0ZfPiPOlTeWB6TfVml2Yzw%3D%3D",
     },
+
     {
       title:
         "Concurrency Models in Node.js: Single-Threaded, Multi-Threaded, and Multi-Process Configurations",
@@ -39,13 +34,32 @@
         "JavaScript, including Node.js, primarily operates on a single-threaded model, but it can handle concurrent operations efficiently through non-blocking I/O, although there are methods available for utilizing multiple threads in Node.js, necessitating an understanding of its concurrency models.",
       url: "https://medium.com/@nirajpaudel/concurrency-models-in-node-js-single-threaded-multi-threaded-and-multi-process-configurations-0080488a1c0b",
     },
-    // {
-    //   title: "5 THINGS I LEARNED FROM ELON MUSK",
-    //   imageUrl: proj4Image,
-    //   description:
-    //     "Elon Musk's groundbreaking achievements underscore the importance of visionary leadership, teamwork, and relentless pursuit of ambitious goals, serving as a powerful inspiration for aspiring entrepreneurs like myself.",
-    //   url: "https://bpazes.com/story/5-things-i-learned-from-elon-musk-884604d4-7468-4998-b90f-8fc7dfeec95c",
-    // },
+    {
+      title: "gRPC Concepts Series on LinkedIn",
+      imageUrl: grpcImage,
+      description:
+        "Explore my LinkedIn post series on gRPC concepts, where I break down key topics and provide insights into using gRPC effectively. Each post covers different aspects of gRPC, offering practical examples and easy-to-understand explanations to help you master this powerful technology.",
+      url: "https://www.linkedin.com/posts/itsmenirajpaudel_backend-grpc-activity-7214582175927525376-dBf7/?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      title: "Protocol Buffers: Understanding Concepts with Examples",
+      imageUrl: protoBufImage,
+      description: `In this article, I explain Protocol Buffers in a simple way. 
+      I cover what they are, how they work, and why they are useful. 
+      You'll learn about their key parts, how to serialize data with them, and 
+      see examples to make everything clear. 
+      This guide is perfect for anyone new to Protocol Buffers or wanting to understand them better"
+     `,
+      url: "https://www.linkedin.com/pulse/prototype-buffers-understanding-concepts-niraj-paudel-g65mc/?trackingId=nPYIcaR9Tau08S6ckuiniw%3D%3D",
+    },
+    {
+      title: "Optimizing JWT Validation in Micro-Services Using API Gateway.",
+      imageUrl: proj7Image,
+      description:
+        "The article discusses integrating API Gateways with JWTs in micro-service architectures for enhanced security and performance.",
+      url: "https://www.linkedin.com/posts/itsmenirajpaudel_jwt-apiabrgateway-microservices-activity-7210939369182887936-V6J_?utm_source=share&utm_medium=member_desktop",
+    },
+
     {
       title: "How JavaScript Directs the Flow of Electricity ?",
       imageUrl: proj5Image,
@@ -54,11 +68,20 @@
       url: "https://www.linkedin.com/pulse/how-javascript-directs-flow-electricity-niraj-paudel-apfrc",
     },
     {
-      title: "Optimizing JWT Validation in Micro-Services Using API Gateway.",
-      imageUrl: proj7Image,
+      title:
+        "Functional Programming for Beginners: Understanding the Core Principles",
+      imageUrl: proj1Image,
       description:
-        "The article discusses integrating API Gateways with JWTs in micro-service architectures for enhanced security and performance.",
-      url: "https://www.linkedin.com/posts/itsmenirajpaudel_jwt-apiabrgateway-microservices-activity-7210939369182887936-V6J_?utm_source=share&utm_medium=member_desktop",
+        "This guide introduces beginners to the principles of Functional Programming (FP), emphasizing the utilization of pure functions, immutability, and function composition for writing cleaner and more maintainable code.",
+      url: "https://medium.com/@nirajpaudel/functional-programming-for-beginners-understanding-the-core-principles-fa8df00966f7",
+    },
+
+    {
+      title: "5 THINGS I LEARNED FROM ELON MUSK",
+      imageUrl: proj4Image,
+      description:
+        "Elon Musk's groundbreaking achievements underscore the importance of visionary leadership, teamwork, and relentless pursuit of ambitious goals, serving as a powerful inspiration for aspiring entrepreneurs like myself.",
+      url: "https://bpazes.com/story/5-things-i-learned-from-elon-musk-884604d4-7468-4998-b90f-8fc7dfeec95c",
     },
   ];
 </script>
